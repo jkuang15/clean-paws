7a1f2713a7250a552344f3960594d7bd8afef8de
Natalie Perrochon <nperroch@uci.edu>
2023-11-09 18:06:47 -0800

    First draft of motor code implemented

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 # main.py
 from roboflow import Roboflow
+import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+import time
+
+
+
 rf = Roboflow(api_key="79w38aK1oCiq60ZrVmG1")
 project = rf.workspace().project("recyclables-ilmxu")
 model = project.version(1).model
+
+def initializePinsAndMotor(): # returns tuple with pin numbers and motor
+    direction_pin_motor = 22 # REPLACE W REAL PIN
+    step_pin_motor = 23 # REPLACE W REAL PIN
+    enable_pin_motor = 24 # REAPLCE W REAL PIN (LOW to enable) 
+    mymotortest = RpiMotorLib.A4988Nema(direction_pin_motor, step_pin_motor, (21,21,21), "DRV8825")
+    GPIO.setup(enable_pin_motor,GPIO.OUT)
+    return direction_pin_motor, step_pin_motor, enable_pin_motor, mymotortest
+
+def moveMotorToPaper(direction_pin_motor, step_pin_motor, enable_pin_motor, mymotortest):
+    GPIO.output(enable_pin_motor, GPIO.LOW)
+    mymotortest.motor_go(False, # True=Clockwise, False=Counter-Clockwise
+                     "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                     200, # number of steps
+                     .0005, # step delay [sec]
+                     False, # True = print verbose output 
+                     .05) # initial delay [sec]
+
 
 
 def print_prediction(lst):
     for pic in image_list:
         print(model.predict(pic, confidence=0, overlap=30).json())
 
+def cleanUpMotor():
+    GPIO.cleanup() 
 
 if __name__ == "__main__":
     image_list = ["images/plastic-bottle-ground.jpeg", "images/plastic-fork.png", "images/water-bottle.jpeg", 
@@ -17,6 +43,13 @@ if __name__ == "__main__":
     # no image list (just one image)
 
     print_prediction(image_list)
+
+
+    # Motor control 
+
+    motor_tuple = initializePinsAndMotor()
+    moveMotorToPaper(motor_tuple)
+    cleanUpMotor()
 
 
 
