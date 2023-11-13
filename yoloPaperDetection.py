c64bf9383e972238e2297cf4fab10dc5e13a99a9
Steven Lee <Stevel20@uci.edu>
2023-11-12 19:24:20 -0800

    adding the files

--- yoloPaperDetection.py
+++ yoloPaperDetection.py
@@ -26,9 +26,54 @@ class PaperDetection:
         """
         Loads the yolov5 model from pytorch hub
         """
-        model_path = "Users/stevenl/yolov5/yolov5/runs/train/yolov5s_results5/weights/best.pt"
         if model_name:
-            model = torch.hub.load('ultralytics/yolov5', 'custom', path = model_name, force_reload = True)
+            model = torch.hub.load('yolov5/', 'custom', path='Users/stevenl/yolov5/yolov5/runs/train/weights/best.pt', source='local') 
         return model
 
+    def score_frame(self, frame):
+        """
+        Takes a single fram as the input and uses the yolov5 model
+        """
+        self.model.to(self.device)
+        frame = [frame]
+        results = self.model(frame)
+        labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
+        return labels, cord
     
+    def class_to_label(self, x):
+        return self.classes[int(x)]
+    
+    def plot_boxes(self, results, frame):
+        labels, cord = results
+        n = len(labels)
+        x_shape, y_shape = frame.shape[1], frame.shape[0]
+        for i in range(n):
+            row = cord[i]
+            if row[4] >= 0.2:
+                x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
+                bgr = (0, 255, 0)
+                cv2.rectangle(frame, (x1, y1), (x2,y2), bgr, 2)
+                cv2.putText(frame, self.class_to_label(labels[i]), (x1,y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
+        
+        return frame
+
+    def __call__(self):
+        player = self.get_video_capture()
+        assert player.isOpened()
+
+        while True:
+            start_time = time()
+            ret, frame = player.read()
+            frame = cv2.resize(frame, (416,416))
+
+            results = self.score_frame(frame)
+            frame = self.plot_boxes(results, frame)
+            end_time = time()
+            cv2.imshow('YOLOv5 Detection', frame)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+        player.release()
+
+detector = PaperDetection(capture_index=0, model_name='best.pt')
+detector()
