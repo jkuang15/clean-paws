122b91b9cdde9abd3bbc54a30b82f320886940ce
Natalie Perrochon <nperroch@uci.edu>
2023-11-16 01:16:25 -0800

    set warnings to false

--- motor2.py
+++ motor2.py
@@ -7,6 +7,7 @@ CW = 1     # Clockwise Rotation
 CCW = 0    # Counterclockwise Rotation
 SPR = 48   # Steps per Revolution (360 / 7.5)
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIR, GPIO.OUT)
 GPIO.setup(STEP, GPIO.OUT)
@@ -15,18 +16,21 @@ GPIO.output(DIR, CW)
 step_count = SPR
 delay = .0208
 
+
 for x in range(step_count):
     GPIO.output(STEP, GPIO.HIGH)
     sleep(delay)
     GPIO.output(STEP, GPIO.LOW)
     sleep(delay)
-
-sleep(.5)
-GPIO.output(DIR, CCW)
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
-
 GPIO.cleanup()
+
+
+# sleep(.5)
+# GPIO.output(DIR, CCW)
+# for x in range(step_count):
+#     GPIO.output(STEP, GPIO.HIGH)
+#     sleep(delay)
+#     GPIO.output(STEP, GPIO.LOW)
+#     sleep(delay)
+
+
