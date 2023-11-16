261949613302bf41938758d70864de35e42cba46
Elise Ji <jie1@uci.edu>
2023-11-15 20:59:30 -0800

    ultrasonic code written, not tested

---
+++ software_to_hardware/sense_funcs.py
@@ -0,0 +1,41 @@
+#Raspberry Pi 4.0 code for interfacing with ultrasonic sensor
+
+import RPi.GPIO as GPIO #importing modules required in program
+import time #to add delays 
+
+GPIO.setmode (GPIO.BCM) 
+
+
+def move(tri, ech):
+
+
+    GPIO.setup(tri, GPIO.OUT) #trig pin is output
+    GPIO.setup(ech, GPIO.IN) #echo pin is input
+    GPIO.OUTPUT(tri, GPIO.LOW) #drives trig pin to OV
+
+    time.sleep(2) #delay 2 secs
+
+    GPIO.output(tri, GPIO.HIGH) #set trig pin high
+
+    time.sleep(0.00001)
+
+    GPIO.output(tri, GPIO.LOW)
+
+    while GPIO.input(ech) == 0:
+        send = time.time() 
+
+    while GPIO.input(ech) == 1:
+        receive = time.time()
+
+    duration = receive - send 
+
+    duration = round(duration/2, 2)
+
+    distance = 34000* duration 
+
+    print("obj at" + distance + "from ultra sensor")
+
+    GPIO.cleanup()
+
+    return distance
+
---
+++ software_to_hardware/ultra_sensors.py
@@ -0,0 +1,18 @@
+# main for ultra sensors
+
+from software_to_hardware import sense_funcs
+
+#1
+sense_funcs.move(15, 16)
+
+#2
+sense_funcs.move(11, 12)
+
+#3
+sense_funcs.move(29, 31)
+
+
+
+
+
+
