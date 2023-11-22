345971126a98462a9b1b31d20201747ccf8f8136
Jamie Kuang <jkuang9@uci.edu>
2023-11-22 02:25:34 -0800

    added text and attempted to add list items

--- website_try2/app.py
+++ website_try2/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +10,16 @@ def landing():
 @app.route("/bin_capacity")
 def bin_capacity():
   return render_template("bin_capacity.html")
+
+@app.route("/")
+def hardware_names():
+  names = ['Miguel Murillo', 'Wilson Nguyen', 'Ellen So', 'Arim Song']
+  return render_template('home.html', names=names)
+
+# @app.route("/")
+# def software_names():
+#   names = ['Elise Ji', 'Jamie Kuang', 'Steven Lee', 'Natalie Perrochon']
+#   return render_template('home.html', names=names)
 
 if __name__ == "__main__":
   app.run(debug=True)
