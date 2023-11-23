05fa6f6e4c8bb941e78bc9abc8bcdf375b3daf77
Natalie Perrochon <nperroch@uci.edu>
2023-11-22 23:23:11 -0800

    cleaned app.py

--- website_try2/app.py
+++ website_try2/app.py
@@ -15,16 +15,6 @@ def bin_capacity():
     photos = ph.get_all_photos()
     return render_template("bin_capacity.html", photos=photos)
 
-@app.route("/")
-def hardware_names():
-  names = ['Miguel Murillo', 'Wilson Nguyen', 'Ellen So', 'Arim Song']
-  return render_template('home.html', names=names)
-
-# @app.route("/")
-# def software_names():
-#   names = ['Elise Ji', 'Jamie Kuang', 'Steven Lee', 'Natalie Perrochon']
-#   return render_template('home.html', names=names)
-
 if __name__ == "__main__":
     app.run(debug=True)
 
