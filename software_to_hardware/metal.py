d811b49a3c6e8d7c184981024c1f4d5d7622904c
Elise Ji <jie1@uci.edu>
2023-11-23 01:08:48 -0800

    modified metal detector function. needs testing for mic device with actual webcam

--- software_to_hardware/metal.py
+++ software_to_hardware/metal.py
@@ -2,30 +2,42 @@ import sounddevice as sd
 import numpy as np
 import soundfile as sf
 # pip install sounddevice
-def db(data):
-    rms = np.sqrt(np.mean(data**2))
-    reference = 1.0 #based on mic sensitivity
-    decibel_level = 20* np.log(rms/reference)
-    return decibel_level
-
-#settings
-duration = 5
-sample_rate = 44100 #HZ?
-filename = "recording.wav"
-
-#capture audio
-audio_record = sd.rec(int(duration * sample_rate), sample_rate = sample_rate, channels = 1, dtype =  'int16')
-
-#saving to WAV file
-
-sf.write(filename, audio_record, sample_rate)
-
-#calculate decibel level
-decibel_level = db(audio_record)
 
 
+def get_decibels(dev):
 
-if 0 < decibel_level < 10:
+    #how long i want to record for in sec:
+    duration = 5
+
+    #samples/sec
+    rate = 44100 #HZ?
+
+    chunk_size = int(duration*rate)
+
+
+    #capture audio
+    audio_rec = sd.rec(chunk_size, sample_rate = rate, channels = 1, dtype = np.int16, device = dev)
+    sd.wait()
+
+    #RMS - getting root mean quare of audio signal: 
+    rms = np.sqrt(np.mean(np.square(audio_rec)))
+
+    #convert RMS to decibels: 
+    results = 20 * np.log(rms)
+
+    return results
+
+
+#/////////////
+# mic is where it's getting the audio from; usually 1 but not always guaranteed to be constant. // needs testing
+mic = 1
+decibels = get_decibels(mic)
+
+
+
+
+
+if 0 < decibels < 10:
     print("no")
 
 else:
