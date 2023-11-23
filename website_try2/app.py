e9fc74a11f844e6abde15af9fbad58a9007294cc
Natalie Perrochon <nperroch@uci.edu>
2023-11-22 23:08:27 -0800

    trying photo thing

--- website_try2/app.py
+++ website_try2/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
+import photo_handler as ph
 
 app = Flask(__name__)
 
 @app.route("/")
 def landing():
-  return render_template("home.html")
+    return render_template("home.html")
 
 @app.route("/bin_capacity")
 def bin_capacity():
-  return render_template("bin_capacity.html")
+    photos = ph.get_all_photos()
+    return render_template("bin_capacity.html", photos=photos)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
 
