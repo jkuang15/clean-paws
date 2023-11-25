f21fd3be877ba2aab0625b68027c795260f2d66b
Elise Ji <jie1@uci.edu>
2023-11-25 00:20:49 -0800

    found and fixed another bug with converting rms to decbiels

--- software_to_hardware/metal.py
+++ software_to_hardware/metal.py
@@ -1,5 +1,6 @@
 import sounddevice as sd
 import numpy as np
+import time
 # import soundfile as sf
 # pip install sounddevice
 
@@ -13,24 +14,36 @@ def get_decibels(dev):
     rate = 44100 #HZ?
 
     chunk_size = int(duration*rate)
+    #chunk_size = 1024
 
 
     #capture audio
     audio_rec = sd.rec(chunk_size, samplerate = rate, channels = 1, dtype = np.int16, device = dev)
     sd.wait()
 
+    if not audio_rec.any():
+        print("Empty data. Skipping calculation.")
+        
+
     #RMS - getting root mean quare of audio signal: 
-    rms = np.sqrt(np.mean(np.square(audio_rec)))
+    rms = np.sqrt(np.mean(np.square(np.abs(audio_rec))))
+
+    if rms <= 0:
+        print("Invalid RMS value. Skipping calculation.")
+
+
 
     #convert RMS to decibels: 
-    results = 20 * np.log(rms)
+    results = 20 * np.log10(rms)
+
+    time.sleep(1)
 
     return results
 
 
 #/////////////
 # mic is where it's getting the audio from; usually 1 but not always guaranteed to be constant. // needs testing
-mic = 1
+mic = 0
 decibels = get_decibels(mic)
 
 
@@ -38,7 +51,13 @@ decibels = get_decibels(mic)
 
 # for testing
 if decibels > 60:
+    print(decibels)
     print("yawr")
 
 else:
+    print(decibels)
     print("nawr")
+
+
+
+
