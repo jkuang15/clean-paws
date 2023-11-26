400fed391fd3861a4544c4520db5fd8dade779f4
Natalie Perrochon <nperroch@uci.edu>
2023-11-26 11:33:24 -0800

    motor4 implemented based on what wilson sent

---
+++ motor4.py
@@ -0,0 +1,19 @@
+import time
+import RPi.GPIO as GPIO
+
+CW = 1
+CCW = 0
+Dir = 14
+Stp = 15
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Dir, GPIO.OUT)
+GPIO.setup(Stp, GPIO.OUT)
+
+GPIO.output(Dir, CW)
+
+for i in range(1000):
+    GPIO.output(Stp, GPIO.HIGH)
+    time.sleep(.01)
+    GPIO.output(Stp, GPIO.LOW)
+    time.sleep(.01)
