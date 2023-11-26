179a420fae2b78d9a976baf41634c3df6d8f963a
Steven Lee <Stevel20@uci.edu>
2023-11-26 15:17:19 -0800

    updated motor 3

--- motor3.py
+++ motor3.py
@@ -19,12 +19,12 @@ import time
 ################################
 #
 #define GPIO pins
-direction= 22 # Direction (DIR) GPIO Pin
-step = 23 # Step GPIO Pin
+direction= 18 # Direction (DIR) GPIO Pin
+step = 16 # Step GPIO Pin
 EN_pin = 24 # enable pin (LOW to enable)
 
 # Declare a instance of class pass GPIO pins numbers and the motor type
-mymotortest = RpiMotorLib.A4988Nema(direction, step, (21,21,21), "DRV8825")
+mymotortest = RpiMotorLib.A4988Nema(direction, step, (18,16,21), "DRV8825")
 GPIO.setup(EN_pin,GPIO.OUT) # set enable pin as output
 
 ###########################
